be50e3d0817b8d1cf6fbf824e466f58482837209
duri12 <eyalgor102@gmail.com>
2022-05-21 21:33:00 +0300

    fix the  .h and added constractor

--- HealthPoints.h
+++ HealthPoints.h
@@ -1,8 +1,12 @@
 #ifndef MTM_EX3_HEALTHPOINTS_H
 #define MTM_EX3_HEALTHPOINTS_H
+class HealthPoints {
+    private:
+        int m_currentHealth;
+        int m_maxHealth;
+    private:
+        explicit HealthPoints(int maxHealth = 100);
+
+};
 
 #endif //MTM_EX3_HEALTHPOINTS_H
- class HealthPoints {
-    int m_current_health;
-    int m_max_health;
-};
