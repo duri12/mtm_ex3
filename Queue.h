8a3e67799f965605abe96b23d2a1a5d99c601ad7
duri12 <eyalgor102@gmail.com>
2022-05-22 17:43:14 +0300

    created the queue.h

--- Queue.h
+++ Queue.h
@@ -1,9 +1,10 @@
 #ifndef MTM_EX3_QUEUE_H
 #define MTM_EX3_QUEUE_H
+#include <iostream>
 template <typename T>
 class Queue{
     public:
-
+    class EmptyQueue{};
     private:
     class Node{
         T* m_value;
@@ -12,5 +13,6 @@ class Queue{
     Node m_Last;
     Node m_First;
     int Length;
+
 };
 #endif //MTM_EX3_QUEUE_H
