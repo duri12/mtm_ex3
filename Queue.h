15c824ad59547f4f71db13c0f0e4716d203f4f96
duri12 <eyalgor102@gmail.com>
2022-05-22 17:30:06 +0300

    created the queue.h

---
+++ Queue.h
@@ -0,0 +1,14 @@
+#ifndef MTM_EX3_QUEUE_H
+#define MTM_EX3_QUEUE_H
+class Queue{
+    public:
+
+    private:
+    class Node{
+
+    };
+    Node m_Last;
+    Node m_First;
+    int Length;
+};
+#endif //MTM_EX3_QUEUE_H
